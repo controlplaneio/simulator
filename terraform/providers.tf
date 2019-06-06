43d48c549332d89765c55c0a8bac00fb12018b3c
jondkent <jon.kent@gmail.com>
2019-06-06 14:05:29 +0100

    added comments

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,3 +1,7 @@
+#
+# Do not hard code credentails in this file
+# Do not place aws credentails file into this repo
+#
 provider "aws" {
   region                  = "${var.region}"
   shared_credentials_file = "${var.shared_credentials_file}"
