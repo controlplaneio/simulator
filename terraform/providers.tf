f916113a94cdd98bac7ddc5ddb0c6c28bede8b17
jondkent <jon.kent@gmail.com>
2019-06-06 15:14:44 +0100

    typo fix

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -5,6 +5,6 @@
 provider "aws" {
   region                  = "${var.region}"
   shared_credentials_file = "${var.shared_credentials_file}"
-  profile                 = "${var.aws_profile}"" 
+  profile                 = "${var.aws_profile}" 
 }
 
