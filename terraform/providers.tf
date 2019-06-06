a2650a4dae52381b74cb3d1210f4dc700bb018b6
jondkent <jon.kent@gmail.com>
2019-06-06 14:00:58 +0100

    added region, cred reference, and awsprofile

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -1,4 +1,6 @@
 provider "aws" {
-  region = "us-west-1"
+  region                  = "${var.region}"
+  shared_credentials_file = "${var.shared_credentials_file}"
+  profile                 = "${var.aws_profile}"" 
 }
 
