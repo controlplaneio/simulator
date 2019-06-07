87f32335c050f05d7cbbbf4f0cb3b3a27b9b5472
jondkent <jon.kent@gmail.com>
2019-06-07 16:05:06 +0100

    updated to support multiple creations of VMs

--- terraform/k8s-controlplane-vm.tf
+++ terraform/k8s-controlplane-vm.tf
@@ -1,13 +1,10 @@
 resource "aws_instance" "controlplane" {
+  count                       = "${var.number_of_controlplane_instances}"
   ami                         = "${var.ami_id}"
   key_name                    = "${aws_key_pair.bastion_key.key_name}"
-  instance_type               = "${var.instance_type}"
+  instance_type               = "${var.controlplane_instance_type}"
   security_groups             = ["${aws_security_group.controlplane-sg.id}"]
   associate_public_ip_address = false
   subnet_id                   = "${aws_subnet.private_subnet.id}"
 }
 
-resource "aws_key_pair" "controlplane_key" {
-  key_name   = "controlplane_key"
-  public_key = "${var.access_key}"
-}
