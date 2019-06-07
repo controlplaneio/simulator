1a501010a3fa5fc4b2a51211a6aec7da9bbaecf0
jondkent <jon.kent@gmail.com>
2019-06-07 16:27:30 +0100

    added more defaults

--- terraform/variables.tf
+++ terraform/variables.tf
@@ -5,7 +5,7 @@ variable "region" {
 }
 
 variable "shared_credentials_file" {
-  description = "location of aws credentiala file"
+  description = "location of aws credentials file"
 }
 
 variable "aws_profile" {
@@ -19,6 +19,7 @@ variable "access_key" {
 
 variable "vpc_cidr" {
   description = "cidr block for vpc"
+  default = "172.31.0.0/16"
 }
 
 variable "access_cidr" {
@@ -27,14 +28,17 @@ variable "access_cidr" {
 
 variable "public_subnet_cidr" {
   description = "cidr range for public subnet"
+  default = "172.31.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "cidr range for private subnet"
+  default = "172.31.2.0/24"
 }
 
 variable "ami_id" {
   description = "ami to use with Bastion host"
+  default = "ami-09d38086eb2b23925"
 }
 
 variable "instance_type" {
@@ -54,9 +58,11 @@ variable "number_of_controlplane_instances" {
 
 variable "private_avail_zone" {
   description = "availability zone for private subnet"
+  default = "eu-west-1a"
 }
 
 variable "public_avail_zone" {
   description = "availability zone for public subnet"
+  default = "eu-west-1a"
 }
 
