2cf748317314ef4192b0588a63c6261f5c947117
jondkent <jon.kent@gmail.com>
2019-06-10 14:49:22 +0100

    renamed clusterNodes to cluster_nodes for consistency

--- terraform/cluster-nodes.tf
+++ terraform/cluster-nodes.tf
@@ -1,8 +1,8 @@
-resource "aws_instance" "clusterNodes" {
+resource "aws_instance" "cluster_nodes" {
   count                       = "${var.number_of_cluster_instances}"
   ami                         = "${var.ami_id}"
   key_name                    = "${aws_key_pair.bastion_key.key_name}"
-  instance_type               = "${var.clusterNodes_instance_type}"
+  instance_type               = "${var.cluster_nodes_instance_type}"
   security_groups             = ["${aws_security_group.controlplane-sg.id}"]
   associate_public_ip_address = false
   subnet_id                   = "${aws_subnet.private_subnet.id}"
