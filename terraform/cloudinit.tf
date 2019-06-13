addbcdf0a6f3ca96a151eba61c00dcaede6f1693
jondkent <jon.kent@gmail.com>
2019-06-13 16:12:48 +0100

    cloud init template

---
+++ terraform/cloudinit.tf
@@ -0,0 +1,22 @@
+
+data "template_file" "init-script" {
+  template = "${file("cloud-init/cloud-init.cfg")}"
+  vars {
+    REGION = "${var.region}"
+  }
+}
+
+
+data "template_cloudinit_config" "cloudinit-example" {
+
+  gzip = false
+  base64_encode = false
+  
+  part {
+    filename     = "cloud-init.cfg"
+    content_type = "text/cloud-config"
+    content      = "${data.template_file.init-script.rendered}"
+  }
+
+}
+
