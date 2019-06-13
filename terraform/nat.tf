85a9429f9f5d4d477e4c743a71ea8fbd33f55387
Jon Kent <jonathankent@ubuntu.us-east1-b.c.geonosis.internal>
2019-06-13 10:41:08 +0000

    updated  tf-auto-doc script and terraform nat.tf template

--- terraform/nat.tf
+++ terraform/nat.tf
@@ -1,5 +1,5 @@
 resource "aws_nat_gateway" "securus_nat" {
     allocation_id = "${aws_eip.securus_eip.id}"
-    subnet_id = "${aws_subnet.public_subnet.id}"
+    subnet_id = "${aws_subnet.private_subnet.id}"
     depends_on = ["aws_internet_gateway.securus_igw"]
 }
