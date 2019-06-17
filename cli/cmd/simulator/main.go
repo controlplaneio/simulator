9ccc1c4530c89b56f57ec90f03370f97ed20eb84
Raoul Millais <raoul@raoulmillais.com>
2019-06-17 16:32:11 +0100

    feat: walking skeleton cli app
    - Simple go cli app that has a single version command
    - Build and test infrastructure

---
+++ cli/cmd/simulator/main.go
@@ -0,0 +1,100 @@
+package main
+
+import (
+	"fmt"
+	"github.com/spf13/cobra"
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"log"
+	"os"
+	"strings"
+)
+
+var (
+	logger *zap.SugaredLogger
+
+	// vars injected by goreleaser at build time
+	version = "unknown"
+	commit  = "unknown"
+	date    = "unknown"
+)
+
+var rootCmd = &cobra.Command{
+	Use:   "simulator",
+	Short: "Simulator command line",
+	Long: `
+A distributed systems and infrastructure simulator for attacking and
+debugging Kubernetes
+`,
+}
+
+func main() {
+	var err error
+
+	// logger writes to stderr
+	logger, err = NewLogger("info", "console")
+	if err != nil {
+		log.Fatalf("can't initialize zap logger: %v", err)
+	}
+
+	defer logger.Sync()
+
+	rootCmd.SetArgs(os.Args[1:])
+	if err := rootCmd.Execute(); err != nil {
+		e := err.Error()
+
+		fmt.Println(strings.ToUpper(e[:1]) + e[1:])
+		os.Exit(1)
+	}
+}
+
+func NewLogger(logLevel string, zapEncoding string) (*zap.SugaredLogger, error) {
+	level := zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	switch logLevel {
+	case "debug":
+		level = zap.NewAtomicLevelAt(zapcore.DebugLevel)
+	case "info":
+		level = zap.NewAtomicLevelAt(zapcore.InfoLevel)
+	case "warn":
+		level = zap.NewAtomicLevelAt(zapcore.WarnLevel)
+	case "error":
+		level = zap.NewAtomicLevelAt(zapcore.ErrorLevel)
+	case "fatal":
+		level = zap.NewAtomicLevelAt(zapcore.FatalLevel)
+	case "panic":
+		level = zap.NewAtomicLevelAt(zapcore.PanicLevel)
+	}
+
+	zapEncoderConfig := zapcore.EncoderConfig{
+		TimeKey:        "timestamp",
+		LevelKey:       "severity",
+		NameKey:        "logger",
+		CallerKey:      "caller",
+		MessageKey:     "message",
+		StacktraceKey:  "stacktrace",
+		LineEnding:     zapcore.DefaultLineEnding,
+		EncodeLevel:    zapcore.LowercaseLevelEncoder,
+		EncodeTime:     zapcore.ISO8601TimeEncoder,
+		EncodeDuration: zapcore.SecondsDurationEncoder,
+		EncodeCaller:   zapcore.ShortCallerEncoder,
+	}
+
+	zapConfig := zap.Config{
+		Level:       level,
+		Development: false,
+		Sampling: &zap.SamplingConfig{
+			Initial:    100,
+			Thereafter: 100,
+		},
+		Encoding:         zapEncoding,
+		EncoderConfig:    zapEncoderConfig,
+		OutputPaths:      []string{"stderr"},
+		ErrorOutputPaths: []string{"stderr"},
+	}
+
+	logger, err := zapConfig.Build()
+	if err != nil {
+		return nil, err
+	}
+	return logger.Sugar(), nil
+}
