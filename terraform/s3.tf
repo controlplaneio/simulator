ad9d4d55c735fea7b8102a7556c02bfe932d4376
jondkent <jon.kent@gmail.com>
2019-06-18 15:50:32 +0100

    added force_destroy for s3 bucket

--- terraform/s3.tf
+++ terraform/s3.tf
@@ -1,6 +1,7 @@
 resource "aws_s3_bucket" "k8sjoin" {
-  bucket = "securus-config"
-  acl    = "private"
+  bucket        = "securus-config"
+  acl           = "private"
+  force_destroy = true
 
   tags = {
     Name        = "K8S Config"
