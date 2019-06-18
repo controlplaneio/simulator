94b6cb63331e5e52cd3a74e54861d826a04b8078
jondkent <jon.kent@gmail.com>
2019-06-18 11:03:40 +0100

    debug policy errors

--- terraform/s3-host-access-iam.tf
+++ terraform/s3-host-access-iam.tf
@@ -19,50 +19,36 @@ EOF
 }
 
 
-resource "aws_iam_policy" "securus_s3_access_policy" {
+resource "aws_iam_role_policy" "securus_s3_access_policy" {
   name        = "securus_s3_host_access_policy"
   role        = "${aws_iam_role.securus_s3_access_role.id}"
-  description = "Host based access to S3 bucket"
 
   policy = <<EOF
-
+{
+  "Version": "2012-10-17",
+  "Statement": [
     {
-     "Version": "2012-10-17",
-     "Statement": [
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:ListBucket"
-             ],
-          "Resource": [
-             "arn:aws:s3:::securus-config"
-             ]
-        },
-        {
-          "Effect": "Allow",
-          "Action": [
-            "s3:PutObject",
-            "s3:GetObject",
-            "s3:DeleteObject",
-            "s3:ListObject"
-             ],
-          "Resource": [
-             "arn:aws:s3:::securus-config/*"
-             ]
-        }
-      ]
+      "Effect": "Allow",
+      "Action": ["s3:ListBucket"],
+      "Resource": ["arn:aws:s3:::securus-config"]
+    },
+    {
+      "Effect": "Allow",
+      "Action": [
+        "s3:PutObject",
+        "s3:GetObject",
+        "s3:DeleteObject"
+      ],
+      "Resource": ["arn:aws:s3:::securus-config/*"]
     }
-EOF
+  ]
 }
-
-resource "aws_iam_role_policy_attachment" "attach_role" {
-  role       = "${aws_iam_role.securus_s3_access_role.name}"
-  policy_arn = "${aws_iam_policy.securus_s3_access_policy.arn}"
+EOF
 }
 
 resource "aws_iam_instance_profile" "instance_profile" {
   name = "instance_profile"
-  roles = ["securus_s3_access_role"]
+  role = "securus_s3_access_role"
 }
 
 
