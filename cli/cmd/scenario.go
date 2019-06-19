44c6225d5ac11003a05c30bd6448f01d441e71fa
Raoul Millais <raoul@raoulmillais.com>
2019-06-19 09:00:28 +0100

    feat: choose scenario and add more tests

--- cli/cmd/scenario.go
+++ cli/cmd/scenario.go
@@ -12,16 +12,28 @@ func newScenarioCommand() *cobra.Command {
 		Use:   `scenario`,
 		Short: "Lists available scenarios",
 		RunE: func(cmd *cobra.Command, args []string) error {
+			if len(args) < 1 {
+				return fmt.Errorf("scenario name is required")
+			}
+
+			name := args[0]
+
 			scenarios, err := loadScenarios(scenariosPath)
 
 			if err != nil {
 				return err
 			}
 
+			fmt.Println("Available scenarios")
 			for _, s := range scenarios {
-				fmt.Println(s.DisplayName)
+				fmt.Println(s.DisplayName + ": " + s.Name)
 			}
 
+			if !contains(scenarios, name) {
+				return fmt.Errorf("scenario %s not found", name)
+			}
+
+			fmt.Println("Chosen scenario")
 			return nil
 		},
 	}
