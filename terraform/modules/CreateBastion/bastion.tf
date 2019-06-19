b2eeb6d0df70e27645fe03234e72e008dc6b7b9f
jondkent <jon.kent@gmail.com>
2019-06-19 13:40:42 +0100

    moved into own branch

--- terraform/modules/CreateBastion/bastion.tf
+++
@@ -1,9 +0,0 @@
-resource "aws_instance" "bastion" {
-  ami                         = "${var.ami_id}"
-  key_name                    = "${aws_key_pair.bastion_key.key_name}"
-  instance_type               = "${var.instance_type}"
-  security_groups             = ["${aws_security_group.bastion-sg.id}"]
-  associate_public_ip_address = true
-  subnet_id                   = "${aws_subnet.public_subnet.id}"
-}
-
--- terraform/modules/Networking/igw.tf
+++
@@ -1,6 +0,0 @@
-resource "aws_internet_gateway" "securus_igw" {
-  vpc_id = "${aws_vpc.securus_vpc.id}"
-  tags = {
-        Name = "Securus InternetGateway"
-    }
-}
