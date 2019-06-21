f5d0a331dc439bbe9dfc672fd140230829442b3d
Raoul Millais <raoul@raoulmillais.com>
2019-06-21 12:24:07 +0100

    test: manifest path
    - Use testing/assertion library
    - Fix loadScenario test
    - Add tests for manifestPath
    - Build before running unit tests

--- cli/cmd/loader_test.go
+++ cli/cmd/loader_test.go
@@ -1,16 +1,27 @@
 package cmd
 
 import (
+	"github.com/stretchr/testify/assert"
+	"os"
 	"testing"
 )
 
 func Test_loadScenarios(t *testing.T) {
-	scenarios, err := loadScenarios("../../simulation-scripts/scenario")
-	if err != nil {
-		t.Fatal(err.Error())
-	}
+	scenarios, err := loadScenarios("../../simulation-scripts/")
 
-	if len(scenarios) == 0 {
-		t.Errorf("Returned no scenarios")
-	}
+	assert.Nil(t, err)
+	assert.NotEqual(t, len(scenarios), 0, "Returned no scenarios")
 }
+
+func Test_manifestPath_default(t *testing.T) {
+	p := manifestPath()
+	assert.Equal(t, p, "../simulation-scripts/")
+}
+
+func Test_manifestPath_custom(t *testing.T) {
+	os.Setenv("SIMULATOR_MANIFEST_PATH", "/some/path")
+	p := manifestPath()
+	assert.Equal(t, p, "/some/path", "manifestPath did not set custom path")
+}
+
+// TODO: Test bad scenario manifests
--- cli/go.mod
+++ cli/go.mod
@@ -3,6 +3,7 @@ module github.com/controlplaneio/simulator-standalone/cli
 require (
 	github.com/pkg/errors v0.8.1
 	github.com/spf13/cobra v0.0.5
+	github.com/stretchr/testify v1.3.0
 	go.uber.org/atomic v1.4.0 // indirect
 	go.uber.org/multierr v1.1.0 // indirect
 	go.uber.org/zap v1.10.0
