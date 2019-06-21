23532911c9ead5669057df1a14c462b013ce06dc
Raoul Millais <raoul@raoulmillais.com>
2019-06-21 11:11:48 +0100

    feat: scenario list manifest file
    - The scenarios to display are now specified with a manifest file named "scenarios.yaml" this allows the hosted and
    standalone simulators to choose independently which scenarios are available.  We can also choose not to make all
    scenarios available - so e.g. the noop test scenario is not in the list
    - Add lots more error checking and wrap errors with more context in loader.go

--- cli/cmd/loader_test.go
+++ cli/cmd/loader_test.go
@@ -14,32 +14,3 @@ func Test_loadScenarios(t *testing.T) {
 		t.Errorf("Returned no scenarios")
 	}
 }
-
-func Test_displayName(t *testing.T) {
-	simpleName := displayName("foo")
-	if simpleName != "Foo" {
-		t.Errorf("Did not make simple name title case")
-	}
-
-	compoundName := displayName("hack_the_world")
-	if compoundName != "Hack The World" {
-		t.Errorf("Did not make compound name title case replacing the underscores")
-	}
-}
-
-func Test_makeScenario(t *testing.T) {
-	dirName := "lazy_AWS"
-	s := makeScenario(dirName)
-
-	if s.DirName != dirName {
-		t.Errorf("Did not set DirName to supplied argument")
-	}
-
-	if s.DisplayName != "Lazy AWS" {
-		t.Errorf("Did not set DisplayName correctly")
-	}
-
-	if s.Id != "lazy_aws" {
-		t.Errorf("Did not set ID correctly")
-	}
-}
