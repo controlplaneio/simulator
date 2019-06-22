7d39c6b119cf7e4842bfdff2c2da6463fb0cabb7
Raoul Millais <raoul@raoulmillais.com>
2019-06-22 21:10:48 +0100

    docs: autogenerate README

---
+++ cli/README.template.md
@@ -0,0 +1,17 @@
+# Simulator CLI
+
+## Usage
+
+<pre>
+${help}
+</pre>
+
+### Scenarios
+
+<pre>
+${scenario_help}
+</pre>
+
+## API Documentation
+
+* [Scenario](./docs/scenario.md)
---
+++ cli/bin/generate-readme.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+#
+# Prelude - make bash behave sanely
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+#
+set -euo pipefail
+IFS=$'\n\t'
+# Beware of CDPATH gotchas causing cd not to work correctly when a user has
+# set this in their environment
+# https://bosker.wordpress.com/2012/02/12/bash-scripters-beware-of-the-cdpath/
+unset CDPATH
+
+#
+# Main function
+#
+main() {
+  local -r template="$(cat ./README.template.md)"
+  local -r help="$(./dist/simulator help)"
+  local -r scenario_help="$(./dist/simulator scenario help)"
+
+  eval "echo \"${template}\""
+}
+
+main
