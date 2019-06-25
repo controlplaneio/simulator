c23f3b31fecf84128d9de8ae8a5a528ad9c94aaf
jondkent <jon.kent@gmail.com>
2019-06-25 16:03:19 +0100

    tidy up

--- terraform/modules/AWS/CreateK8s/variables.tf
+++ terraform/modules/AWS/CreateK8s/variables.tf
@@ -1,26 +1,26 @@
 variable "region" {
   description = "aws region"
-  default = "eu-west-1"
+  default     = "eu-west-1"
 }
 variable "number_of_master_instances" {
   description = "number of master instances to create"
-  default = "1"
+  default     = "1"
 }
 variable "ami_id" {
   description = "ami to use"
-  default = "ami-09d38086eb2b23925"
+  default     = "ami-09d38086eb2b23925"
 }
 variable "master_instance_type" {
   description = "instance type for master node(s) "
-  default = "t2.medium"
+  default     = "t2.medium"
 }
 variable "number_of_cluster_instances" {
   description = "number of nodes to create"
-  default = "1"
+  default     = "1"
 }
 variable "cluster_nodes_instance_type" {
   description = "instance type for k8s nodes"
-  default = "t1.micro"
+  default     = "t1.micro"
 }
 variable "key_pair_name" {
   description = "Name of ssh key held in KMS"
