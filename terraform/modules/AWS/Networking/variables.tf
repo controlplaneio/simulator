f687336668d22e207694d76a36c88e6056af2acb
jondkent <jon.kent@gmail.com>
2019-06-25 16:00:53 +0100

    tidy up

--- terraform/modules/AWS/Networking/variables.tf
+++ terraform/modules/AWS/Networking/variables.tf
@@ -1,24 +1,24 @@
 variable "vpc_cidr" {
   description = "cidr block for vpc"
-  default = "172.31.0.0/16"
+  default     = "172.31.0.0/16"
 }
 
 variable "public_subnet_cidr" {
   description = "cidr range for public subnet"
-  default = "172.31.1.0/24"
+  default     = "172.31.1.0/24"
 }
 
 variable "private_subnet_cidr" {
   description = "cidr range for private subnet"
-  default = "172.31.2.0/24"
+  default     = "172.31.2.0/24"
 }
 
 variable "private_avail_zone" {
   description = "availability zone for private subnet"
-  default = "eu-west-1a"
+  default     = "eu-west-1a"
 }
 
 variable "public_avail_zone" {
   description = "availability zone for public subnet"
-  default = "eu-west-1a"
+  default     = "eu-west-1a"
 }
