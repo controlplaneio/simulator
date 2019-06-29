a608037ded42d327ec159e067abb9dc60395ce66
Raoul Millais <raoul@raoulmillais.com>
2019-06-29 11:26:14 +0100

    test: test MakePerturbOptions

--- cli/pkg/runner/perturb_test.go
+++ cli/pkg/runner/perturb_test.go
@@ -16,3 +16,23 @@ func Test_ToArguments_And_String(t *testing.T) {
 
 	assert.Equal(t, po.String(), "--master 127.0.0.1 --slaves 8.8.8.8")
 }
+
+func Test_MakePerturbOptions(t *testing.T) {
+	t.Parallel()
+	tfo := runner.TerraformOutput{
+		MasterNodesPrivateIP: runner.StringSliceOutput{
+			Value: []string{"127.0.0.1"},
+		},
+		ClusterNodesPrivateIP: runner.StringSliceOutput{
+			Value: []string{"127.0.0.2"},
+		},
+	}
+
+	path := "./scenario/test"
+
+	po := runner.MakePerturbOptions(tfo, path)
+
+	assert.Equal(t, po.Master.String(), tfo.MasterNodesPrivateIP.Value[0])
+	assert.Equal(t, po.Slaves[0].String(), tfo.ClusterNodesPrivateIP.Value[0])
+
+}
