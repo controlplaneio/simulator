84ec596c3e1201ccde8b7e97cd43e289a1a4bcc4
Raoul Millais <raoul@raoulmillais.com>
2019-07-01 07:18:20 +0100

    test: add Slurp test

--- pkg/util/util_test.go
+++ pkg/util/util_test.go
@@ -8,6 +8,10 @@ import (
 	"time"
 )
 
+func fixture(name string) string {
+	return "../../test/fixtures/" + name
+}
+
 func Test_FileExists_current_file(t *testing.T) {
 	exists, err := util.FileExists("./util_test.go")
 
@@ -55,4 +59,10 @@ func Test_ExpandTilde(t *testing.T) {
 	// Call ExpandTilde again to exercise the cache
 	p2, err := util.ExpandTilde("~/.")
 	assert.Equal(t, *p, *p2, "Cached version differed")
+}
+
+func Test_Slurp(t *testing.T) {
+	contents, err := util.Slurp(fixture("tf-help.txt"))
+	assert.Nil(t, err, "Got an error")
+	assert.NotEmpty(t, contents, "Got empty file contents")
 }
