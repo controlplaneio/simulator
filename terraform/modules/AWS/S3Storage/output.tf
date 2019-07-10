4591394d07f72c00b574f20bbeddf3c577968765
jondkent <jon.kent@gmail.com>
2019-07-10 11:08:52 +0100

    additional output

--- terraform/modules/AWS/S3Storage/output.tf
+++ terraform/modules/AWS/S3Storage/output.tf
@@ -2,3 +2,12 @@ output "IamInstanceProfileId" {
   value       = "${aws_iam_instance_profile.simulator_instance_profile.id}"
   description = "IAM instance profile id"
 }
+output "IamAccessRoleName" {
+  value       = "${aws_iam_role.simulator_s3_access_role.name}"
+  description = "IAM simulator s3 access role"
+}
+
+output "S3BucketName" {
+  value       = "${aws_s3_bucket.k8sjoin.id}"
+  description = "Name of S3 bucket created"
+}
