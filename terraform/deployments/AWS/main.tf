93035ebdc1ad2f0393815ed01bbe61a4389496cc
jondkent <jon.kent@gmail.com>
2019-07-11 13:50:43 +0100

    moved keypair to separate module

--- terraform/deployments/AWS/main.tf
+++ terraform/deployments/AWS/main.tf
@@ -8,12 +8,17 @@ module "Networking" {
   private_avail_zone         = "${var.private_avail_zone}"
 }
 
+module "SshKey" {
+  source                      = "../../modules/AWS/SshKey"
+  access_key_name             = "${var.access_key_name}"
+  access_key                  = "${var.access_key}"
+}
+
 module "Bastion" {
   source                      = "../../modules/AWS/Bastion"
   ami_id                      = "${var.ami_id}"
   instance_type               = "${var.instance_type}"
-  access_key_name             = "${var.access_key_name}"
-  access_key                  = "${var.access_key}"
+  access_key_name             = "${module.SshKey.KeyPairName}"
   security_group              = "${module.SecurityGroups.BastionSecurityGroupID}"
   subnet_id                   = "${module.Networking.PublicSubnetId}"
   master_ip_addresses         = "${ join(",", "${module.Kubernetes.K8sMasterPrivateIp}")}"
@@ -28,7 +33,7 @@ module "Kubernetes" {
   master_instance_type        = "${var.master_instance_type}"
   number_of_cluster_instances = "${var.number_of_cluster_instances}"
   cluster_nodes_instance_type = "${var.cluster_nodes_instance_type}"
-  key_pair_name               = "${module.Bastion.KeyPairName}"
+  access_key_name             = "${module.SshKey.KeyPairName}"
   control_plane_sg_id         = "${module.SecurityGroups.ControlPlaneSecurityGroupID}"
   private_subnet_id           = "${module.Networking.PrivateSubnetId}"
   iam_instance_profile_id     = "${module.S3Storage.IamInstanceProfileId}"
