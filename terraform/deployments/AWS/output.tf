c0a55448ffe2c12eb6202411f14c97a3b188f8ec
Jon Kent <jon.kent@gmail.com>
2019-07-17 22:21:11 +0100

    added missing descriptions

--- terraform/deployments/AWS/output.tf
+++ terraform/deployments/AWS/output.tf
@@ -1,16 +1,21 @@
 output "bastion_public_ip" {
-  value = "${module.Bastion.BastionPublicIp}"
+  value       = "${module.Bastion.BastionPublicIp}"
+  description = "Bastion public IP"
 }
 output "master_nodes_private_ip" {
-  value = "${module.Kubernetes.K8sMasterPrivateIp}"
+  value       = "${module.Kubernetes.K8sMasterPrivateIp}"
+  description = "Master node private IP"
 }
 output "cluster_nodes_private_ip" {
-  value = "${module.Kubernetes.K8sNodesPrivateIp}"
+  value       = "${module.Kubernetes.K8sNodesPrivateIp}"
+  description = "Cluster node private IPs"
 }
 output "access_cidr" {
-  value = "${var.access_cidr}"
+  value       = "${var.access_cidr}"
+  description = "Remote access IP"
 }
 output "ami_id" {
-  value =  "${module.Ami.AmiId}"
+  value       =  "${module.Ami.AmiId}"
+  description = "AMI used for all instances"
 }
 
