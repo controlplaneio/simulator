1d4a13cc3e7a3b1bc670d3b721c111d07e8acd37
Raoul Millais <raoul@raoulmillais.com>
2019-07-19 12:05:46 +0100

    fix(infra): stop tf always recreating VMs
    - See https://github.com/hashicorp/terraform/issues/7853

--- terraform/modules/AWS/Bastion/main.tf
+++ terraform/modules/AWS/Bastion/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "simulator_bastion" {
   ami                         = "${var.ami_id}"
   key_name                    = "${var.access_key_name}"
   instance_type               = "${var.instance_type}"
-  security_groups             = ["${var.security_group}"]
+  vpc_security_group_ids      = ["${var.security_group}"]
   associate_public_ip_address = true
   subnet_id                   = "${var.subnet_id}"
   user_data                   = "${data.template_file.cloud_config.rendered}"
--- terraform/modules/AWS/Kubernetes/main.tf
+++ terraform/modules/AWS/Kubernetes/main.tf
@@ -3,7 +3,7 @@ resource "aws_instance" "simulator_master_instances" {
   ami                         = "${var.ami_id}"
   key_name                    = "${var.access_key_name}"
   instance_type               = "${var.master_instance_type}"
-  security_groups             = ["${var.control_plane_sg_id}"]
+  vpc_security_group_ids      = ["${var.control_plane_sg_id}"]
   associate_public_ip_address = false
   subnet_id                   = "${var.private_subnet_id}"
   user_data                   = "${data.template_file.master_cloud_config.rendered}"
@@ -15,7 +15,7 @@ resource "aws_instance" "simulator_node_instances" {
   ami                         = "${var.ami_id}"
   key_name                    = "${var.access_key_name}"
   instance_type               = "${var.cluster_nodes_instance_type}"
-  security_groups             = ["${var.control_plane_sg_id}"]
+  vpc_security_group_ids      = ["${var.control_plane_sg_id}"]
   associate_public_ip_address = false
   subnet_id                   = "${var.private_subnet_id}"
   user_data                   = "${data.template_file.node_cloud_config.rendered}"
