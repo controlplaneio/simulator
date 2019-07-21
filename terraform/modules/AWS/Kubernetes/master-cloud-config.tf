2ccba08f66e35ad03f4b02822c5772b293f7bfcd
Raoul Millais <raoul@raoulmillais.com>
2019-07-21 15:09:20 +0100

    feat(infra): disable ubuntu motd and show hostname

--- terraform/modules/AWS/Kubernetes/master-cloud-config.tf
+++ terraform/modules/AWS/Kubernetes/master-cloud-config.tf
@@ -2,7 +2,7 @@ data "template_file" "master_cloud_config" {
   count = "${var.number_of_master_instances}"
   template = "${file("${path.module}/master-cloud-config.yaml")}"
   vars = {
-    hostname = "kubernetes-master-${count.index}"
+    hostname = "k8s-master-${count.index}"
     s3_bucket_name = "${var.s3_bucket_name}"
   }
 }
--- terraform/modules/AWS/Kubernetes/node-cloud-config.tf
+++ terraform/modules/AWS/Kubernetes/node-cloud-config.tf
@@ -2,7 +2,7 @@ data "template_file" "node_cloud_config" {
   count = "${var.number_of_cluster_instances}"
   template = "${file("${path.module}/node-cloud-config.yaml")}"
   vars = {
-    hostname = "kubernetes-node-${count.index}"
+    hostname = "k8s-node-${count.index}"
     s3_bucket_name = "${var.s3_bucket_name}"
   }
 }
