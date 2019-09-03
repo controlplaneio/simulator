45097aadd58422f2f89cfa98126a239f695d72a2
Raoul Millais <raoul@raoulmillais.com>
2019-09-03 09:58:53 +0100

    test: goss test for kubernetes nodes

--- terraform/modules/AWS/Kubernetes/test.tf
+++ terraform/modules/AWS/Kubernetes/test.tf
@@ -31,3 +31,37 @@ resource "null_resource" "master_test" {
     ]
   }
 }
+
+resource "null_resource" "node_test" {
+  count = "${var.number_of_node_instances}"
+
+  connection {
+    bastion_host        = "${var.bastion_public_ip}"
+    bastion_private_key = "${file(pathexpand("~/.ssh/cp_simulator_rsa"))}"
+    host                = "${element(aws_instance.simulator_node_instances.*.private_ip, count.index)}"
+    type                = "ssh"
+    user                = "root"
+    // disable ssh-agent support
+    agent       = "false"
+    private_key = "${file(pathexpand("~/.ssh/cp_simulator_rsa"))}"
+    // Increase the timeout so the server has time to reboot
+    timeout = "10m"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/../../scripts/run-goss.sh"
+    destination = "/root/run-goss.sh"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/node-goss.yaml"
+    destination = "/root/goss.yaml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /root/run-goss.sh",
+      "/root/run-goss.sh",
+    ]
+  }
+}
