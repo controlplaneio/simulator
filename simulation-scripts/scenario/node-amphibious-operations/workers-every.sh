4815a7303c5bbe3e4adf14089a807218c081b4cf
pi-unnerup <pi.unnerup@gmail.com>
2019-09-20 10:36:22 +0100

    feat: kubelet unauth scenario

---
+++ simulation-scripts/scenario/node-amphibious-operations/workers-every.sh
@@ -0,0 +1,9 @@
+add-apt-repository ppa:rmescandon/yq
+apt update
+apt install yq -y
+
+yq w -i /var/lib/kubelet/config.yaml authentication.anonymous.enabled true
+yq w -i /var/lib/kubelet/config.yaml authorization.mode AlwaysAllow
+
+systemctl daemon-reload
+systemctl restart kubelet.service
