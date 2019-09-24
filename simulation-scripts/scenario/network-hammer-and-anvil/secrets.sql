fa56dfc8dbf439ce35ba497df74606d5e4218d2e
Sam Irvine <s-irvine@users.noreply.github.com>
2019-09-24 11:30:23 +0100

    feat: network hammer and anvil scenario completed

--- simulation-scripts/scenario/network-hammer-and-anvil/secrets.sql
+++ simulation-scripts/scenario/network-hammer-and-anvil/secrets.sql
@@ -1,2 +1,17 @@
-CREATE TABLE secrets (secret     CHAR(50)       NOT NULL);
-INSERT INTO secrets VALUES ('notthesecretsyouarelookingfor');
+CREATE TABLE personaldata (
+  id SERIAL UNIQUE NOT NULL,
+  name VARCHAR(10) NOT NULL,
+  creditcard TEXT,
+  address TEXT NOT NULL,
+  postcode VARCHAR(6) NOT NULL
+);
+
+INSERT INTO personaldata (
+    name, creditcard, address, postcode
+)
+SELECT
+    left(md5(i::text), 10),
+    md5(random()::text),
+    md5(random()::text),
+    left(md5(random()::text), 6)
+FROM generate_series(1, 1000000) s(i)
