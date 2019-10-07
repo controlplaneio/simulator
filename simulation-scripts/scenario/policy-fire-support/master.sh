f389da825c992c486e2f9e3de76926baa8f390e5
Sam Irvine <s-irvine@users.noreply.github.com>
2019-10-07 10:27:21 +0100

    fix: peturb scripts now run on correct hosts

---
+++ simulation-scripts/scenario/policy-fire-support/master.sh
@@ -0,0 +1,58 @@
+#!/bin/bash
+
+kubectl apply -f - <<EOF
+apiVersion: policy/v1beta1
+kind: PodSecurityPolicy
+metadata:
+  name: admin
+  annotations:
+    seccomp.security.alpha.kubernetes.io/allowedProfileNames: '*'
+spec:
+  privileged: true
+  allowPrivilegeEscalation: true
+  allowedCapabilities:
+  - '*'
+  volumes:
+  - '*'
+  hostNetwork: true
+  hostPorts:
+  - min: 0
+    max: 65535
+  hostIPC: true
+  hostPID: true
+  runAsUser:
+    rule: 'RunAsAny'
+  seLinux:
+    rule: 'RunAsAny'
+  supplementalGroups:
+    rule: 'RunAsAny'
+  fsGroup:
+    rule: 'RunAsAny'
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: ClusterRole
+metadata:
+  name: admin-psp
+rules:
+- apiGroups: ['policy']
+  resources: ['podsecuritypolicies']
+  verbs:     ['use']
+  resourceNames:
+  - admin
+---
+apiVersion: rbac.authorization.k8s.io/v1
+kind: RoleBinding
+metadata:
+  name: admin-psp
+  namespace: kube-system
+roleRef:
+  kind: ClusterRole
+  name: admin-psp
+  apiGroup: rbac.authorization.k8s.io
+subjects:
+  - kind: Group
+  apiGroup: rbac.authorization.k8s.io
+  name: system:serviceaccounts
+EOF
+
+sed -i '/    - --enable-admission-plugins=NodeRestriction/c\    - --enable-admission-plugins=NodeRestriction,PodSecurityPolicy' /etc/kubernetes/manifests/kube-apiserver.yaml
