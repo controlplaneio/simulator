f75048248a60fe0f44aaf7c27e265bc2e86ffeb1
jondkent <jon.kent@gmail.com>
2019-10-17 13:44:58 +0100

    feat: added internal node module to main.tf

--- terraform/deployments/AWS/main.tf
+++ terraform/deployments/AWS/main.tf
@@ -50,6 +50,17 @@ module "Kubernetes" {
   default_tags                = "${var.default_tags}"
 }
 
+// Setup host within Kubernetes subnet
+module "InternalNode" {
+  source              = "../../modules/AWS/InternalNode"
+  ami_id              = "${module.Ami.AmiId}"
+  instance_type       = "${var.instance_type}"
+  access_key_name     = "${module.SshKey.KeyPairName}"
+  security_group      = "${module.SecurityGroups.ControlPlaneSecurityGroupID}"
+  subnet_id           = "${module.Networking.PrivateSubnetId}"
+  default_tags        = "${var.default_tags}"
+}
+
 // Create S3 bucket to share Kubernetes join details between
 // master and nodes
 module "S3Storage" {
