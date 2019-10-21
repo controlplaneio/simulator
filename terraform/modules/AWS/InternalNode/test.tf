e0db43c4fe0fb41cb2b1095b4e5257880794bfbf
jondkent <jon.kent@gmail.com>
2019-10-21 12:15:49 +0100

    feat:add goss tests

---
+++ terraform/modules/AWS/InternalNode/test.tf
@@ -0,0 +1,32 @@
+resource "null_resource" "internal_node_test" {
+
+  connection {
+    host                      = "${aws_instance.simulator_bastion.public_ip}"
+    internal_node_private_key = "${file(pathexpand("~/.ssh/cp_simulator_rsa"))}"
+    host                      = "${aws_instance.simulator_internal_node.private_ip}"
+    type                      = "ssh"
+    user                      = "root"
+    // disable ssh-agent support
+    agent       = "false"
+    private_key = "${file(pathexpand("~/.ssh/cp_simulator_rsa"))}"
+    // Increase the timeout so the server has time to reboot
+    timeout = "10m"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/../../scripts/run-goss.sh"
+    destination = "/root/run-goss.sh"
+  }
+
+  provisioner "file" {
+    source      = "${path.module}/goss.yaml"
+    destination = "/root/goss.yaml"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "chmod +x /root/run-goss.sh",
+      "/root/run-goss.sh",
+    ]
+  }
+}
