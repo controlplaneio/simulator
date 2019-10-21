b282b84ea9cd1997b0c6b3d7e3de66997d497b26
Sam Irvine <s-irvine@users.noreply.github.com>
2019-10-21 16:36:30 +0100

    fix: node-shock-tactics keys permissions and challenge.txt

--- simulation-scripts/scenario/node-shock-tactics/workers-every.sh
+++ simulation-scripts/scenario/node-shock-tactics/workers-every.sh
@@ -1,4 +1,5 @@
 useradd -u 12345 -g users -d /home/alex -s /bin/bash -p alexisgreat alex
 mkdir -pv /home/alex/.ssh
 cp /root/.ssh/authorized_keys /home/alex/.ssh/authorized_keys
+chown alex /home/alex/.ssh/authorized_keys
 chmod a+rw /etc/shadow
