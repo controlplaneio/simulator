046c65f67144629e15b4209964586221a787b495
pi-unnerup <pi.unnerup@gmail.com>
2019-10-29 15:43:49 +0000

    feat: master-bait-and-bleed done

--- simulation-scripts/scenario/master-bait-and-bleed/master.sh
+++ simulation-scripts/scenario/master-bait-and-bleed/master.sh
@@ -5,5 +5,5 @@ apt update
 apt install -y yq
 apt-get install -y jq
 
-yq r -j /etc/kubernetes/manifests/kube-apiserver.yaml | jq --arg auth "--insecure-port=0" '(.spec.containers[].command[] | select(. == $auth)) = "--insecure-port=8080"' | yq r - > kube-apiserver.yaml
+yq r -j /etc/kubernetes/manifests/kube-apiserver.yaml | jq --arg auth "--insecure-port=0" '(.spec.containers[].command[] | select(. == $auth)) = "--insecure-port=8080"' | jq '.spec.containers[].command += ["--insecure-bind-address=0.0.0.0"]' | yq r - > kube-apiserver.yaml
 mv kube-apiserver.yaml /etc/kubernetes/manifests/kube-apiserver.yaml
