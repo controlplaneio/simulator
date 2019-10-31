a4fab83f7589e577a2777ede2b5e7ee81a80c5c6
Raoul Millais <raoul@raoulmillais.com>
2019-10-31 18:52:16 +0000

    docs: add code docs for transforming hints

--- tools/migrate-hints/bin/transform-hints.js
+++ tools/migrate-hints/bin/transform-hints.js
@@ -1,22 +1,22 @@
 #!/usr/bin/env node
 
-// Usage: run this in the root of the simulator dir
-//
-// `npm install js-yaml`
-// `node transform-hints.js`
-//
+// Usage: run `make devtools` and then `migrate-hints` in root of the simulator dir
+
 const yaml = require('js-yaml')
 const {truncateSync, writeFileSync, readFileSync, readdirSync} = require('fs')
 const {resolve,join} = require('path')
 const {transformV0ToV1} = require('../lib/transforms')
 
-// Mixing sync and async but it doesnt matter as this is a quick n dirty script
+// Loads and parses a `hints.yaml` from the supplied absolute path. 
+// Returns an object representing the yaml file
 function loadHintsFile(p) {
   console.log(`Loading ${p}`)
   const doc = yaml.safeLoad(readFileSync(p, 'utf8'));
   return doc
 }
 
+// Serializes the supplied `hints` to YAML and overwrites an existing 
+// `hints.yaml` file to the supplied path `p` with the YAMl
 function writeHintsFile(hints, p) {
   console.log(`Writing transformed file ${p}`)
   const contents = yaml.safeDump(hints)
@@ -24,6 +24,8 @@ function writeHintsFile(hints, p) {
   writeFileSync(p, contents)
 }
 
+// Given a relative path to a scenario directory, scans for scenarios
+// Returns a list of absolute paths to all `hints.yaml` files
 function findScenarioHintsFiles(scenariosDir) {
   const absPath = resolve(scenariosDir)
 
