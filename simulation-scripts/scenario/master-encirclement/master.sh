4f1240adf71a8d7d8d0748c1dd42717fc70d8131
Sam Irvine <s-irvine@users.noreply.github.com>
2019-10-31 16:02:17 +0000

    feat: master-encirclement scenario

---
+++ simulation-scripts/scenario/master-encirclement/master.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+git clone https://github.com/controlplaneio/kubesec-webhook.git webhook &> /dev/null
+
+cd webhook
+
+apt update && apt install -y make &> /dev/null
+
+make certs &> /dev/null
+
+make deploy &> /dev/null
+
+kubectl create namespace master-encirclement &> /dev/null
+
+kubectl label namespaces master-encirclement kubesec-validation=enabled &> /dev/null
+
+sed -i '18i\ \ \ \ \- --disable-admission-plugins=MutatingAdmissionWebhook' /etc/kubernetes/manifests/kube-apiserver.yaml
