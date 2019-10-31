fd18f078daed94df0b0d909970a144d25c3bdb7d
Raoul Millais <raoul@raoulmillais.com>
2019-10-31 20:15:18 +0000

    test: test io of devtools
    - refactor for testability
    - add tests for loading and writing yaml

--- tools/migrate-hints/bin/transform-hints.js
+++ tools/migrate-hints/bin/transform-hints.js
@@ -2,37 +2,8 @@
 
 // Usage: run `make devtools` and then `migrate-hints` in root of the simulator dir
 
-const yaml = require('js-yaml')
-const {truncateSync, writeFileSync, readFileSync, readdirSync} = require('fs')
-const {resolve,join} = require('path')
+const {loadHintsFile, writeHintsFile, findScenarioHintsFiles} = require('../lib/io')
 const {transformV0ToV1} = require('../lib/transforms')
-
-// Loads and parses a `hints.yaml` from the supplied absolute path. 
-// Returns an object representing the yaml file
-function loadHintsFile(p) {
-  console.log(`Loading ${p}`)
-  const doc = yaml.safeLoad(readFileSync(p, 'utf8'));
-  return doc
-}
-
-// Serializes the supplied `hints` to YAML and overwrites an existing 
-// `hints.yaml` file to the supplied path `p` with the YAMl
-function writeHintsFile(hints, p) {
-  console.log(`Writing transformed file ${p}`)
-  const contents = yaml.safeDump(hints)
-  truncateSync(p)
-  writeFileSync(p, contents)
-}
-
-// Given a relative path to a scenario directory, scans for scenarios
-// Returns a list of absolute paths to all `hints.yaml` files
-function findScenarioHintsFiles(scenariosDir) {
-  const absPath = resolve(scenariosDir)
-
-  return readdirSync(absPath, { withFileTypes: true })
-    .filter(dirent => dirent.isDirectory())
-    .map(dirent => join(absPath, dirent.name, 'hints.yaml'))
-}
 
 hintsFiles = findScenarioHintsFiles('./simulation-scripts/scenario')
 hintsFiles.forEach(hintsFile => {
--- tools/migrate-hints/test/transforms_test.js
+++ tools/migrate-hints/test/transforms_test.js
@@ -7,13 +7,14 @@ test('transforms v0 to v1 schema', t => {
         objective: "test objective",
         "starting-point": "kubectl pod exec.",
         "num-hints": "2"
-      }, {
+      }
+    }, {
       hints: {
         "hint-1": "test hint 1",
         "hint-2": "test hint 2"
       }
     }
-  }]
+  ]
 
   const expected = [{
       kind: "cp.simulator/scenario:1.0.0",
@@ -21,14 +22,10 @@ test('transforms v0 to v1 schema', t => {
       general_overview: {
         objective: "test objective",
         "starting-point": "kubectl pod exec."
-      }, {
-    hints: [ {
-        "text": "test hint 1"
-      }, {
-        "text": "test hint 2"
       }
+    }, {
+    hints: [ { "text": "test hint 1" }, { "text": "test hint 2" } ]
     }]
-  }
 
   t.deepEqual(transformV0ToV1(original), expected)
 })
