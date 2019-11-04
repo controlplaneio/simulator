60f75cebd1ef54ddca9a8c424a29874ce7b4da9e
Raoul Millais <raoul@raoulmillais.com>
2019-11-04 16:39:39 +0000

    feat: wire up entrypoint to arg parsing

--- tools/scenario-tools/bin/scenario.js
+++ tools/scenario-tools/bin/scenario.js
@@ -1,6 +1,10 @@
 #!/usr/bin/env node
 
+const os = require('os')
+
 const { createLogger } = require('../lib/logger')
+const { parse, showUsage } = require('../lib/args')
+const { migrate } = require('../lib/migrate')
 
 // const args = process.argv.slice(2)
 const logger = createLogger({})
@@ -26,3 +30,37 @@ process.on('unhandledRejection', (err) => {
 
   process.exit(1)
 })
+
+// Only show dev options when not in the attack container
+function showHelp () {
+  let dev = false
+  if (os.hostname() !== 'attack') {
+    dev = true
+  }
+
+  showUsage(dev)
+}
+
+function cloneArray (arr) {
+  return arr.slice(0)
+}
+
+const args = cloneArray(process.argv)
+
+args.shift() // remove `node` from argv
+args.shift() // remove `scenario.js` from argv
+console.log(args)
+try {
+  const { command, options } = parse(args)
+
+  // TODO(rem): Implement show-hints and next-hint
+  if (command === 'migrate') {
+    migrate(options)
+    process.exit(0)
+  } else {
+    showHelp()
+  }
+} catch {
+  logger.error('Unrecognised cli arguments - try running the \'help\' command')
+  showHelp()
+}
