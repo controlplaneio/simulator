a6441ffc09d236825e85b53f2cd046051caef0bc
Sam Irvine <s-irvine@users.noreply.github.com>
2019-11-19 16:36:51 +0000

    style(infra): conform comments in providers.tf to spec

--- terraform/deployments/AWS/providers.tf
+++ terraform/deployments/AWS/providers.tf
@@ -7,8 +7,10 @@ provider "aws" {}
 terraform {
   backend "s3" {
     key = "simulator.tfstate"
-    bucket = "###REPLACED-BY-SIMULATOR###" # Must have exact number of spaces for simulator to replace properly
-    encrypt = false # Optional, S3 Bucket Server Side Encryption
+    // 'bucket='' must have this exact number of spaces for simulator to replace it properly
+    bucket = "###REPLACED-BY-SIMULATOR###"
+    // Optional, S3 Bucket Server Side Encryption
+    encrypt = false
   }
 }
 
@@ -16,7 +18,9 @@ data "terraform_remote_state" "state" {
   backend = "s3"
   config  = {
     key     = "simulator.tfstate"
-    bucket = "###REPLACED-BY-SIMULATOR###"  # Must have exact number of spaces for simulator to replace properly
-    encrypt = false # Optional, S3 Bucket Server Side Encryption
+    // 'bucket='' must have this exact number of spaces for simulator to replace it properly
+    bucket = "###REPLACED-BY-SIMULATOR###"
+    // Optional, S3 Bucket Server Side Encryption
+    encrypt = false
   }
 }
