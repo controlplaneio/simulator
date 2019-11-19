188c63e4ba4317fbfae7c8007e83a952d43b3907
Sam Irvine <s-irvine@users.noreply.github.com>
2019-11-19 14:17:38 +0000

    feat(infra): tag AWS resources with the simulator bucket name

--- terraform/deployments/AWS/providers.tf
+++ terraform/deployments/AWS/providers.tf
@@ -7,9 +7,16 @@ provider "aws" {}
 terraform {
   backend "s3" {
     key = "simulator.tfstate"
-    bucket = "###REPLACED-BY-SIMULATOR###"
+    bucket = "###REPLACED-BY-SIMULATOR###" # Must have exact number of spaces for simulator to replace properly
     encrypt = false # Optional, S3 Bucket Server Side Encryption
   }
 }
 
-
+data "terraform_remote_state" "state" {
+  backend = "s3"
+  config  = {
+    key     = "simulator.tfstate"
+    bucket = "###REPLACED-BY-SIMULATOR###"  # Must have exact number of spaces for simulator to replace properly
+    encrypt = false # Optional, S3 Bucket Server Side Encryption
+  }
+}
