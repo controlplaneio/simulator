6e94c9efe1b78d3185fa3e8c18447ac853ba5082
pi-unnerup <pi.unnerup@gmail.com>
2019-12-13 16:25:25 +0000

    feat: no write to providers.tf

--- pkg/simulator/terraform_vars.go
+++ pkg/simulator/terraform_vars.go
@@ -1,12 +1,12 @@
 package simulator
 
 import (
-	"fmt"
+	// "fmt"
 	"github.com/controlplaneio/simulator-standalone/pkg/util"
-	"github.com/pkg/errors"
-	"io/ioutil"
-	"path/filepath"
-	"strings"
+	// "github.com/pkg/errors"
+	// "io/ioutil"
+	// "path/filepath"
+	// "strings"
 )
 
 // TfVars struct representing the input variables for terraform to create the
@@ -28,28 +28,28 @@ func NewTfVars(publicKey, accessCIDR, bucketName, attackTag string) TfVars {
 	}
 }
 
-func writeProvidersFile(tfDir, bucket string) error {
-	providerspath := filepath.Join(tfDir, "providers.tf")
-	input, err := ioutil.ReadFile(providerspath)
-	if err != nil {
-		return errors.Wrapf(err, "Error reading providers file %s", providerspath)
-	}
+// func writeProvidersFile(tfDir, bucket string) error {
+// 	providerspath := filepath.Join(tfDir, "providers.tf")
+// 	input, err := ioutil.ReadFile(providerspath)
+// 	if err != nil {
+// 		return errors.Wrapf(err, "Error reading providers file %s", providerspath)
+// 	}
 
-	lines := strings.Split(string(input), "\n")
-	for i, line := range lines {
-		if strings.Contains(line, "bucket = ") {
-			lines[i] = fmt.Sprintf("    bucket = \"%s\"", bucket)
-		}
-	}
-	output := strings.Join(lines, "\n")
+// 	lines := strings.Split(string(input), "\n")
+// 	for i, line := range lines {
+// 		if strings.Contains(line, "bucket = ") {
+// 			lines[i] = fmt.Sprintf("    bucket = \"%s\"", bucket)
+// 		}
+// 	}
+// 	output := strings.Join(lines, "\n")
 
-	err = ioutil.WriteFile(providerspath, []byte(output), 0644)
-	if err != nil {
-		return errors.Wrapf(err, "Error writing providers file %s", providerspath)
-	}
+// 	err = ioutil.WriteFile(providerspath, []byte(output), 0644)
+// 	if err != nil {
+// 		return errors.Wrapf(err, "Error writing providers file %s", providerspath)
+// 	}
 
-	return nil
-}
+// 	return nil
+// }
 
 func (tfv *TfVars) String() string {
 	return "access_key = \"" + tfv.PublicKey + "\"\n" + "access_cidr = \"" + tfv.AccessCIDR + "\"\n" + "attack_container_tag = \"" + tfv.AttackTag + "\"\n"
@@ -61,11 +61,11 @@ func EnsureLatestTfVarsFile(tfDir, publicKey, accessCIDR, bucket, attackTag stri
 	filename := tfDir + "/settings/bastion.tfvars"
 	tfv := NewTfVars(publicKey, accessCIDR, bucket, attackTag)
 
-	err := writeProvidersFile(tfDir, bucket)
-	if err != nil {
-		return errors.Wrap(err, "Error saving bucket name")
+	// err := writeProvidersFile(tfDir, bucket)
+	// if err != nil {
+	// 	return errors.Wrap(err, "Error saving bucket name")
 
-	}
+	// }
 
 	return util.OverwriteFile(filename, tfv.String())
 }
--- terraform/deployments/AWS/providers.tf
+++ terraform/deployments/AWS/providers.tf
@@ -8,7 +8,7 @@ terraform {
   backend "s3" {
     key = "simulator.tfstate"
     // 'bucket='' must have this exact number of spaces for simulator to replace it properly
-    bucket = "###REPLACED-BY-SIMULATOR###"
+    //bucket = "###"
     // Optional, S3 Bucket Server Side Encryption
     encrypt = false
   }
