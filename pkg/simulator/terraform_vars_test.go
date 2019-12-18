69c697827c26f024dd3d4f821986dbd2260707d8
pi-unnerup <pi.unnerup@gmail.com>
2019-12-18 11:08:36 +0000

    feat: tests passing

--- pkg/simulator/terraform_vars_test.go
+++ pkg/simulator/terraform_vars_test.go
@@ -7,21 +7,22 @@ import (
 	"testing"
 )
 
-// func Test_TfVars_String(t *testing.T) {
-// 	t.Parallel()
-// 	tfv := simulator.NewTfVars("ssh-rsa", "10.0.0.1/16", "test", "latest")
-// 	expected := `access_key = "ssh-rsa"
-// access_cidr = "10.0.0.1/16"
-// attack_container_tag = "latest"
-// `
-// 	assert.Equal(t, tfv.String(), expected)
-// }
+func Test_TfVars_String(t *testing.T) {
+	t.Parallel()
+	tfv := simulator.NewTfVars("ssh-rsa", "10.0.0.1/16", "test-bucket", "latest")
+	expected := `access_key = "ssh-rsa"
+access_cidr = "10.0.0.1/16"
+attack_container_tag = "latest"
+state_bucket_name = "test-bucket"
+`
+	assert.Equal(t, tfv.String(), expected)
+}
 
 func Test_Ensure_TfVarsFile_with_settings(t *testing.T) {
 	tfDir := fixture("tf-dir-with-settings")
 	varsFile := tfDir + "/settings/bastion.tfVars"
 
-	err := simulator.EnsureLatestTfVarsFile(tfDir, "ssh-rsa", "10.0.0.1/16", "test", "latest")
+	err := simulator.EnsureLatestTfVarsFile(tfDir, "ssh-rsa", "10.0.0.1/16", "test-bucket", "latest")
 	assert.Nil(t, err, "Got an error")
 
 	assert.Equal(t, util.MustSlurp(varsFile), "test = true\n")
