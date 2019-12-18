cc97e13c6e1fbbf2d779e691736837d795498567
pi-unnerup <pi.unnerup@gmail.com>
2019-12-18 16:53:33 +0000

    fix: flag is arg and remove unnecessary comments

--- terraform/deployments/AWS/providers.tf
+++ terraform/deployments/AWS/providers.tf
@@ -7,8 +7,6 @@ provider "aws" {}
 terraform {
   backend "s3" {
     key = "simulator.tfstate"
-    // 'bucket='' must have this exact number of spaces for simulator to replace it properly
-    //bucket = "###"
     // Optional, S3 Bucket Server Side Encryption
     encrypt = false
   }
