df738b4d403d74f15e6d1e5acc78fc0779683946
Sam Irvine <s-irvine@users.noreply.github.com>
2020-01-21 10:41:27 +0000

    fix: move summary field out of hints array

--- simulation-scripts/tools/jq-all-tasks.sh
+++ simulation-scripts/tools/jq-all-tasks.sh
@@ -2,6 +2,6 @@
 
 for dir in simulation-scripts/scenario/*/ ; do
   echo "${dir}"
-  new_yaml=$(yq r -j "${dir}"tasks.yaml | jq '.tasks[].hints |= .+[{"summary": ""}]' | yq r -)
+  new_yaml=$(yq r -j "${dir}"tasks.yaml | jq '.tasks[] |= .+{"summary": ""}' | yq r -)
   echo "${new_yaml}" > "${dir}"tasks.yaml
 done
