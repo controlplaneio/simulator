4ec2a1f4ed66c79f3b3dfa741779a56f8bd08ffa
Sam Irvine <s-irvine@users.noreply.github.com>
2020-02-11 15:32:04 +0000

    fix: remove cat for shellcheck

--- simulation-scripts/tools/add-category-and-difficulty.sh
+++ simulation-scripts/tools/add-category-and-difficulty.sh
@@ -4,7 +4,7 @@
 for dir in simulation-scripts/scenario/*/; do
     echo "${dir}"
     category=$(echo "${dir}" | cut -f3 -d"/" | cut -f1 -d"-")
-    difficulty=$(cat "${dir}"challenge.txt | grep Difficulty | cut -f2 -d":" | tr -d " ")
+    difficulty=$(grep Difficulty "${dir}"challenge.txt | cut -f2 -d":" | tr -d " ")
     echo "${category} ${difficulty}"
     new_yaml=$(yq r -j "${dir}"tasks.yaml | jq --arg CATEGORY "${category}" '. |= .+{"category": $CATEGORY}' | jq --arg DIFFICULTY "${difficulty}" '. |= .+{"difficulty": $DIFFICULTY}' | yq r - | yq r -j - | jq '.' | yq r -)
     echo "${new_yaml}" >"${dir}"tasks.yaml
