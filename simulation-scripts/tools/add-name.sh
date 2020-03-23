9bb6fa733a3412c87d0c2e084b12475d765573ce
Raoul Millais <raoul@raoulmillais.com>
2020-03-23 11:01:49 +0000

    chore: fix shellcheck issues

--- simulation-scripts/tools/add-name.sh
+++ simulation-scripts/tools/add-name.sh
@@ -1,8 +1,10 @@
-
 #! /bin/bash
 # This script will add a name to each tasks file
 for dir in simulation-scripts/scenario/*/; do
     echo "${dir}"
-    new_yaml=$(yq r -j "${dir}"tasks.yaml | jq --arg name "$(basename $dir)" '. + {"name": $name}' | yq r - --prettyPrint)
+    # shellcheck erroneously complains about the jq variable not being double 
+    # quoted - this syntax is correct
+    # shellcheck disable=SC2086
+    new_yaml=$(yq r -j "${dir}"tasks.yaml | jq --arg name "$(basename ${dir})" '. + {"name": $name}' | yq r - --prettyPrint)
     echo "${new_yaml}" >"${dir}"tasks.yaml
 done
