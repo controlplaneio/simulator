f6628f6cd528f418a12ca1fe78768f0f60cfa8ea
Sam Irvine <s-irvine@users.noreply.github.com>
2020-08-17 15:49:04 +0100

    feat: pin terraform provider versions

--- terraform/deployments/AWS/providers.tf
+++ terraform/deployments/AWS/providers.tf
@@ -2,12 +2,24 @@
 // Do not hard code credentails in this file
 // Do not place aws credentails file into this repo
 //
-provider "aws" {}
-
 terraform {
   backend "s3" {
     key = "simulator.tfstate"
     // Optional, S3 Bucket Server Side Encryption
     encrypt = false
+  }
+  required_providers {
+    aws = {
+      version = "~> 2.70"
+    }
+    null = {
+      version = "~> 2.1"
+    }
+    random = {
+      version = "~> 2.3"
+    }
+    template = {
+      version = "~> 2.1"
+    }
   }
 }
