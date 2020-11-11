c6c0b5eafa63e62f22ed3c60b033230d3511d145
Lewis Denham-Parry <lewis@control-plane.io>
2020-11-11 19:53:54 +0000

    WIP: bad certificate for etcd
    - cannot connect to etcd due to bad certificate.
    - added comment until resolved

--- simulation-scripts/scenario/cluster-creds-leak/master.sh
+++ simulation-scripts/scenario/cluster-creds-leak/master.sh
@@ -6,3 +6,5 @@ apt install -y jq yq
 
 yq r -j /etc/kubernetes/manifests/etcd.yaml | jq --arg auth "--client-cert-auth=true" '(.spec.containers[].command[] | select(. == $auth)) = "--client-cert-auth=false"' | yq r - > etcd.yaml
 mv etcd.yaml /etc/kubernetes/manifests/etcd.yaml
+
+# etcdctl can connect on master but externally without certs
