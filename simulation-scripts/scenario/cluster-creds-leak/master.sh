bacf2dbab256402446469eaed2c7812d12e24fb2
Lewis Denham-Parry <lewis@control-plane.io>
2020-11-12 18:05:27 +0000

    Revert "Merge branch 'master' of github.com:controlplaneio/simulator-private"
    This reverts commit 7e6a0f6aaaa50fd7e48f3b68b7d09ce026054a64, reversing
    changes made to 6e1bf26af9ecbecb2237b5fae3d8bdf0720ed66c.

--- simulation-scripts/scenario/cluster-creds-leak/master.sh
+++ simulation-scripts/scenario/cluster-creds-leak/master.sh
@@ -5,5 +5,6 @@ apt update
 apt install -y jq yq
 
 yq r -j /etc/kubernetes/manifests/etcd.yaml | jq --arg auth "--client-cert-auth=true" '(.spec.containers[].command[] | select(. == $auth)) = "--client-cert-auth=false"' | yq r - > etcd.yaml
-sed -i -E -e 's#, "--peer-trusted-ca-file=/etc/kubernetes/pki/etcd/ca.crt"##g' -e 's#, "--trusted-ca-file=/etc/kubernetes/pki/etcd/ca.crt"##g' etcd.yaml 
 mv etcd.yaml /etc/kubernetes/manifests/etcd.yaml
+
+# etcdctl can connect on master but externally without certs
--- tools/scenario-tools/lib/tasks.js
+++ tools/scenario-tools/lib/tasks.js
@@ -184,7 +184,7 @@ async function startTask (newTask, tasks, progress, log, prompter) {
 
 function logTask (newTask, log) {
   if (!process.env.KUBESIM) {
-    // log.info(`You are now on task ${newTask}`)
+    log.info(`You are now on task ${newTask}`)
   }
 }
 
