1b500b5c80384975c8054f4cfb357f063570d70a
Andrew Martin <sublimino@gmail.com>
2021-02-03 01:06:36 +0000

    feat: node-secret-breach

--- simulation-scripts/scenario/spandex-node-secret-breach/workers-every.sh
+++ simulation-scripts/scenario/spandex-node-secret-breach/workers-every.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
 
 mkdir -pv /node-secrets
-echo "node-secret=flag_ctf{7e778526e78d7fdf}" > /node-secrets/secret.txt
+echo "node-secret=flag_ctf{39964a723b9e9b9c}" > /node-secrets/secret.txt
