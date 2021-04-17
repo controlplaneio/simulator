4262d5b41fc1a25163d83bdcf0969775c340da11
Andrew Martin <sublimino@gmail.com>
2021-04-17 10:00:33 +0100

    feat: set up Kubecon EU 21 scenarios

---
+++ simulation-scripts/scenario/kceu21-anon-user-compromise/workers-every.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+add-apt-repository ppa:rmescandon/yq
+apt update
+apt install yq -y
+
+yq w -i /var/lib/kubelet/config.yaml authentication.anonymous.enabled true
+yq w -i /var/lib/kubelet/config.yaml authorization.mode AlwaysAllow
+
+systemctl daemon-reload
+systemctl restart kubelet.service
---
+++ simulation-scripts/scenario/kceu21-node-secret-breach/workers-every.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+mkdir -pv /node-secrets
+echo "node-secret=flag_ctf{39964a723b9e9b9c}" > /node-secrets/secret.txt
