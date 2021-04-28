fe87ad69b33963076c0ede66695783026e7131ed
Andrew Martin <sublimino@gmail.com>
2021-04-28 01:15:33 +0100

    feat: pod ps ns share scenario (escalate uncover)
    - escape to host via mount
    - could be changed to be a symlink

---
+++ simulation-scripts/scenario/kceu21-escalate-uncover-secret/workers-every.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+set -Eeuxo pipefail
+
+mkdir -p /tmp/logs/tmp/logs/abandon/hope/all/ye/arrrrgggh/ere//\!/\[/\[/\$/Hλ$ħ𝔍Ⱥ¢k/\!/\]/\]/\$/\$/\$/\$/
+
+cat <<EOF >> $_/.flag
+flag_ctf{17feeafeadd7833b} # Hλ$ħ𝔍Ⱥ¢k 🏴‍☠️
+EOF
