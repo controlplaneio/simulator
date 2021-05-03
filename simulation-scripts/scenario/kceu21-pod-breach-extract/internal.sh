39f57f887ca7f592ef962da624953aaa2292100a
Andrew Martin <sublimino@gmail.com>
2021-05-03 14:00:05 +0100

    fix: stability for internal host on pod-breach-extract

--- simulation-scripts/scenario/kceu21-pod-breach-extract/internal.sh
+++ simulation-scripts/scenario/kceu21-pod-breach-extract/internal.sh
@@ -7,6 +7,28 @@ IMAGE="control-plane.io/valiant:effort"
 main() {
   setup
 
+  if [[ ! -d /root/.kube ]]; then
+    sleep 30
+    ls -lasp /var/local/.kube || true
+    if [[ ! -d /root/.kube ]]; then
+      sleep 30
+      ls -lasp /var/local/.kube || true
+      if [[ ! -d /root/.kube ]]; then
+        sleep 30
+        ls -lasp /var/local/.kube || true
+        if [[ ! -d /root/.kube ]]; then
+          echo "/root/.kube not found"
+          if [[ -d /var/local/.kube ]]; then
+            ls -lasp /var/local/.kube
+            mv /var/local/.kube /root
+          else
+            exit 1
+          fi
+        fi
+      fi
+    fi
+  fi
+
   # double bind mount secretzy
   SECRET_DIR=$(mktemp -d)
   mount -o bind "${SECRET_DIR}" /root/.kube
