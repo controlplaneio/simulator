b1671c1da996a126b8a815b3ce435fd48e0ad851
James Cleverley-Prance <james.cleverley-prance@control-plane.io>
2021-09-30 10:37:04 +0100

    Add packer dependency

--- packer/k8s-base/scripts/setup.sh
+++ packer/k8s-base/scripts/setup.sh
@@ -8,7 +8,7 @@ rm /tmp/authorized_keys.sh
 sudo apt-get update -y
 sudo DEBIAN_FRONTEND=noninteractive apt-get -y -o Dpkg::Options::="--force-confdef" -o Dpkg::Options::="--force-confold" dist-upgrade
 sudo apt-get update
-sudo apt-get -y -qq install curl wget git vim apt-transport-https ca-certificates
+sudo apt-get -y -qq install curl wget git vim apt-transport-https ca-certificates cloud-init
 
 VERSION='1.20.*'
 cat <<EOF | sudo bash
