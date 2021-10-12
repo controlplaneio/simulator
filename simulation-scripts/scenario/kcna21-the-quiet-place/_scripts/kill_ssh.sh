7f41b1bcd8359f52b9d64bc29386b3b60d360748
James Cleverley-Prance <james.cleverley-prance@control-plane.io>
2021-10-12 13:02:04 +0100

    Bug fixes

--- simulation-scripts/scenario/kcna21-the-quiet-place/_scripts/kill_ssh.sh
+++ simulation-scripts/scenario/kcna21-the-quiet-place/_scripts/kill_ssh.sh
@@ -6,14 +6,11 @@ event=${1:-"$(< /dev/stdin)"}
 
 level=$(echo "$event" | awk '{print $2}')
 
+wall "Bye bye, better luck next time !!!" 2>/dev/null
+sleep 1
+
 PIDS=$(ps aux | grep -v awk | awk '/sshd:/ {print $2}' | tr '\n' ' ')
-
 if [[ "$PIDS" != "" ]]; then
-    echo "killing $PIDS" >> /tmp/falco.log
-    wall "Bye bye, better luck next time !!!" 2>/dev/null
-
-    sleep 1
-    #wall -n "dead"
     kill -9 "$PIDS"
 fi
 
@@ -21,4 +18,7 @@ TOKEN=$(cat /var/lib/kubelet/pods/*/volumes/kubernetes.io~secret/falco-token-*/t
 CA="/etc/kubernetes/pki/ca.crt"
 SERVER=$(awk '/server:/ {print $2}' /etc/kubernetes/kubelet.conf)
 
-kubectl --server="$SERVER" --token="$TOKEN" --certificate-authority="$CA" delete pods,ds -n default --all
+kubectl --server="$SERVER" --token="$TOKEN" --certificate-authority="$CA" delete pods,ds -n default --field-selector='metadata.name!=start'
+kubectl --server="$SERVER" --token="$TOKEN" --certificate-authority="$CA" delete pods,ds -n the-quiet-place --field-selector='metadata.name!=start'
+
+tcpkill port 10250 &>/dev/null & sleep 10; killall tcpkill
