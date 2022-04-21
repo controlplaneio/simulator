d27a20bd1f75b415d7edf9fdbc7f7f07d27847d7
James Cleverley-Prance <james.cleverley-prance@control-plane.io>
2022-04-21 11:08:17 +0100

    feat: change starting user to ubuntu

--- simulation-scripts/scenario/kcna21-back-to-the-future/master.sh
+++ simulation-scripts/scenario/kcna21-back-to-the-future/master.sh
@@ -8,9 +8,8 @@ kind: CertificateSigningRequest
 metadata:
   name: player
 spec:
-  request: $(cat player.csr | base64 | tr -d '\n')
-  usages: ['digital signature', 'key encipherment',
-    'client auth']
+  request: $(base64 -w0 player.csr)
+  usages: ['digital signature', 'key encipherment', 'client auth']
 EOF
 kubectl certificate approve player
 kubectl wait --for=condition=Approved csr player
@@ -21,6 +20,13 @@ kubectl config set-context player --cluster kubernetes --user player
 kubectl config use-context player
 kubectl config delete-context kubernetes-admin@kubernetes
 kubectl config delete-user kubernetes-admin
+cp /root/.kube/config /home/ubuntu/.kube/config
+mkdir -p ~ubuntu/.kube
+chown ubuntu:ubuntu -R ~ubuntu/.kube/
 
 timedatectl set-ntp off
 date --set="25 OCT 1985 07:53:00"
+echo "ubuntu ALL=(ALL) /usr/sbin/hwclock *" > /etc/sudoers.d/time
+gpasswd -d ubuntu sudo
+rm /etc/sudoers.d/90-cloud-init-users
+chmod 755 /etc/sudoers.d
