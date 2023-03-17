71fe96fdf9a19c72c4f1b756f65f1eb05c6ae4fb
James Cleverley-Prance <james.cleverley-prance@control-plane.io>
2023-03-17 10:37:35 +0000

    minor: revert now unnecessary change in #c6231d4

--- terraform/modules/AWS/CloudInitCommon/cloud-config.tf
+++ terraform/modules/AWS/CloudInitCommon/cloud-config.tf
@@ -1,6 +1,6 @@
 locals {
   cc_common = templatefile("${path.module}/cloud-config.yaml", {
-    github_usernames = [for u in var.access_github_usernames : tostring(u) if u != ""]
+    github_usernames = var.access_github_usernames
     generic_bashrc   = filebase64("${path.module}/bashrc")
     generic_inputrc  = filebase64("${path.module}/inputrc")
     generic_aliases  = filebase64("${path.module}/bash_aliases")
