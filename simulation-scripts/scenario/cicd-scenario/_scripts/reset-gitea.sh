8769352fa31fe96d3d974661493f171cad845d49
James Cleverley-Prance <james.cleverley-prance@control-plane.io>
2023-03-22 10:53:24 +0000

    minor: fix reset script order

--- simulation-scripts/scenario/cicd-scenario/_scripts/reset-gitea.sh
+++ simulation-scripts/scenario/cicd-scenario/_scripts/reset-gitea.sh
@@ -10,18 +10,6 @@ BASEURL="http://localhost:8080"
 # shellcheck disable=SC2139
 alias curl="curl -sS -u \"$GITEA_SERVER_USER:$GITEA_SERVER_PASSWORD\" -H 'Content-type: application/json'"
 
-USERS=$(curl "$BASEURL/api/v1/admin/users" | jq '.[].login' -r | tr '\n' ' ')
-
-for USR in $USERS; do
-    if [[ "$USR" == "$GITEA_SERVER_USER" ]]; then
-        continue
-    fi
-    SUCCESS=$(curl "$BASEURL/api/v1/admin/users/$USR" -XDELETE -o /dev/null -w "%{http_code}" )
-    if [[ "$SUCCESS" != "204" ]]; then
-        echo "Deleting $USR failed with $SUCCESS"
-    fi
-done
-
 ORG="rescue-drop"
 REPO="production-image-build"
 SUCCESS=$(curl "$BASEURL/api/v1/repos/$ORG/$REPO" -XDELETE -o /dev/null -w "%{http_code}" )
@@ -38,3 +26,14 @@ SUCCESS=$(curl "$BASEURL/api/v1/users/$GITEA_SERVER_USER/tokens/cli_token" -XDEL
 if [[ "$SUCCESS" != "204" ]]; then
     echo "Deleting user token failed with $SUCCESS"
 fi
+
+USERS=$(curl "$BASEURL/api/v1/admin/users" | jq '.[].login' -r | tr '\n' ' ')
+for USR in $USERS; do
+    if [[ "$USR" == "$GITEA_SERVER_USER" ]]; then
+        continue
+    fi
+    SUCCESS=$(curl "$BASEURL/api/v1/admin/users/$USR" -XDELETE -o /dev/null -w "%{http_code}" )
+    if [[ "$SUCCESS" != "204" ]]; then
+        echo "Deleting $USR failed with $SUCCESS"
+    fi
+done
