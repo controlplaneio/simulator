8fb2da0fe59d2fb83a86460d3c4c713ccb9e4b5f
James Cleverley-Prance <james.cleverley-prance@control-plane.io>
2023-03-23 10:09:02 +0000

    feat: add MASTER_IP interpolation to build action

--- simulation-scripts/scenario/cicd-scenario/05-local.sh
+++ simulation-scripts/scenario/cicd-scenario/05-local.sh
@@ -6,18 +6,19 @@ PASS="9zvB2cQf2tdC"
 ORG="rescue-drop"
 DOMAIN="rescue.drop"
 REPO="production-image-build"
+MASTER_IP="$(get_master)"
 
-SCP_FLAGS="-pr"
-run_scp "./_git-repo-orders-processor/*" "$(get_master):/tmp/gitrepo"
-run_scp "./_git-repo-orders-processor/.github" "$(get_master):/tmp/gitrepo"
+run_scp "./_git-repo-orders-processor/*" "$MASTER_IP:/tmp/gitrepo"
+run_scp "./_git-repo-orders-processor/.github" "$MASTER_IP:/tmp/gitrepo"
 
-run_ssh "$(get_master)" bash <<EOF
+run_ssh "$MASTER_IP" bash <<EOF
 cd /tmp/gitrepo/
 git config --global init.defaultBranch main
 git init
 git config --local user.name "$USER"
 git config --local user.email "$USER@$DOMAIN"
 git remote add localhost http://$USER:$PASS@localhost:30080/$ORG/$REPO
+sed -i -e "s/__REGISTRY_IP__/$MASTER_IP/g" .github/workflows/build.yaml
 git add .
 git commit -m "Initial Commit"
 git push localhost
