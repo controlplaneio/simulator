29d3afd8fc825b660006f79d0d2f7637f2794c51
James Cleverley-Prance <james.cleverley-prance@control-plane.io>
2023-03-23 10:04:51 +0000

    fix: allow setting base action runner image

--- simulation-scripts/scenario/cicd-scenario/03-worker-2.sh
+++ simulation-scripts/scenario/cicd-scenario/03-worker-2.sh
@@ -12,8 +12,6 @@ systemctl disable --now kubelet
 
 apt update && apt install -y --no-install-recommends docker.io
 
-systemctl enable --now docker
-
 # Get Token
 # It's fine to hardcode creds here as this script never touches disk on the node.
 USER="ctf_admin"
@@ -22,6 +20,11 @@ PASS="ahXeehohsoo2suej4tee0ol5xeeteM1w"
 export KUBECONFIG=/etc/kubernetes/kubelet.conf
 MASTER_IP=$(kubectl get nodes k8s-master-0 -ojsonpath='{.status.addresses}' | jq '.[] | select(.type=="InternalIP") | .address' -r)
 BASEURL="http://$MASTER_IP:30080/"
+
+# Configure Insecure docker registry
+systemctl stop docker
+echo "{\"registry-mirrors\":[\"https://mirror.gcr.io\"],\"insecure-registries\":[\"$MASTER_IP:30080\"]}" > /etc/docker/daemon.json
+systemctl enable --now docker
 
 # Gitea may take a minute to come up here; allow 120s
 CSRF=$(curl -sS --fail -c cookie.jar "$BASEURL" --retry 12 --retry-connrefused --retry-delay 10 | awk -F' ' '/csrfToken/ {print $2}' | tr -d "',")
@@ -33,5 +36,5 @@ rm -f /var/lib/kubelet/pki/kubelet-client-current.pem
 shred -fu /var/lib/kubelet/pki/*
 
 touch /tmp/.runner
-docker run --name act-runner -d --restart always -e GITEA_CI_TOKEN="$TOKEN" -e GITEA_CI_URL="$BASEURL" --uts host -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/.runner:/runner/.runner docker.io/controlplaneoffsec/act-runner:latest
-docker pull docker.io/library/node:16-bullseye
+LABELS="ubuntu-latest:docker://ghcr.io/catthehacker/ubuntu:act-latest,ubuntu-22.04:docker://ghcr.io/catthehacker/ubuntu:act-22.04"
+docker run --name act-runner -d --restart always -e GITEA_CI_TOKEN="$TOKEN" -e GITEA_CI_URL="$BASEURL" -e GITEA_CI_LABELS="$LABELS" --uts host -v /var/run/docker.sock:/var/run/docker.sock -v /tmp/.runner:/runner/.runner docker.io/controlplaneoffsec/act-runner:latest
