78c4e7b2945753175c520b326c18af398048d896
Kevin Ward <kevin.wake.ward@gmail.com>
2023-08-22 12:17:00 +0100

    fix: corrected multiple manifest issues, hardening, etc

--- simulation-scripts/scenario/seven-seas/01-worker-2.sh
+++ simulation-scripts/scenario/seven-seas/01-worker-2.sh
@@ -2,39 +2,39 @@
 
 set -Eeuxo pipefail
 
-mkdir -p /opt/.cache/crows-nest
-mkdir -p /opt/.cache/bowsprit
-mkdir -p /opt/.cache/hull
-mkdir -p /opt/.cache/bow/shark-infested-waters
-mkdir -p /opt/.cache/yardarm
-mkdir -p /opt/.cache/poop-deck
-mkdir -p /opt/.cache/stern
-mkdir -p /opt/.cache/helm
-mkdir -p /opt/.cache/ratlines
-mkdir -p /opt/.cache/hold
-mkdir -p /opt/.cache/forecastle/bunk
-mkdir -p /opt/.cache/forecastle/secret-compartment
+mkdir -p /mnt/.cache/crows-nest
+mkdir -p /mnt/.cache/bowsprit
+mkdir -p /mnt/.cache/hull
+mkdir -p /mnt/.cache/bow/shark-infested-waters
+mkdir -p /mnt/.cache/yardarm
+mkdir -p /mnt/.cache/poop-deck
+mkdir -p /mnt/.cache/stern
+mkdir -p /mnt/.cache/helm
+mkdir -p /mnt/.cache/ratlines
+mkdir -p /mnt/.cache/hold
+mkdir -p /mnt/.cache/forecastle/bunk
+mkdir -p /mnt/.cache/forecastle/secret-compartment
 
-cat <<EOF >"/opt/.cache/crows-nest/shinny-object"
+cat <<EOF >"/mnt/.cache/crows-nest/shinny-object"
 eW91LWxvb2stdXAtYXQtdGhlLXNoaW5ueS1vYmplY3QtYW5kLWEtc2VhZ3VsbC1hcHBlYXJzLW91dC1vZi1zdW5saWdodC1kcm9wcGluZy1hLWxpdHRsZS1wcmVzZW50LW9uLXlvdXItZmFjZS5UaGlzLWlzLW5vdC10aGUtcG9vcC1kZWNrLXNjcmFtLXlvdS1mbHlpbmctd3JldGNoIQo=
 EOF
 
-cat <<EOF >"/opt/.cache/bow/shark-infested-waters/shark"
+cat <<EOF >"/mnt/.cache/bow/shark-infested-waters/shark"
 eW91LW11c3QtYmUtY3JhenktdG8tcGV0LWEtc2hhcmsteW91LWhhdmUtbG9zdC15b3VyLWhhbmQtYW5kLWhhdmUtYS1ob29rLW5vdwo=
 EOF
 
-cat <<EOF >"/opt/.cache/hold/treasure-chest"
+cat <<EOF >"/mnt/.cache/hold/treasure-chest"
 YS1tb25rZXktanVtcHMtb3V0LWFuZC1zY3JhdGNoZXMtZmFjZS1hbmQtYml0ZXMteW91ci1ub3NlLW91Y2ghCg==
 EOF
 
-cat <<EOF >"/opt/.cache/forecastle/bunk/blanket"
+cat <<EOF >"/mnt/.cache/forecastle/bunk/blanket"
 d293LXRoZXJlLWFyZS1hLWxvdC1vZi1mbGVhcy1vbi10aGlzLWJsYW5rZXQtYW5kLWEtc291cC1zdGFpbi1hdC1sZWFzdC15b3UtaG9wZS1pdC1pcy1hLXNvdXAtc3RhaW4teXVrIQo=
 EOF
 
-cat <<EOF >"/opt/.cache/crows-nest/stern/adventure-gallery"
+cat <<EOF >"/mnt/.cache/crows-nest/stern/adventure-gallery"
 eW91LWNhbi1zZWUtdGhlLWFkdmVudHVyZS1nYWxsZXJ5LWluLXRoZS1pbmRpYW4tb2NlYW4taWYteW91LWdldC1jbG9zZS1lbm91Z2gteW91LXByb2JhYmx5LWNvdWxkLXNpbmstaXQK
 EOF
 
-cat <<EOF >"/opt/.cache/forecastle/secret-compartment/treasure-map-4"
+cat <<EOF >"/mnt/.cache/forecastle/secret-compartment/treasure-map-4"
 dFVTNjBua01JZ1gzc1A2NVVGWEVESWpVSjkzY1F2ZkxZMFc0ZWVIcllhYzJTWjRqOEtlU0g4d2ZsVFVveTg4T2NGbDdmMwpKUDNvSjFNVlZGVnJIOEw2aU5TTGNBQUFBREFRQUJBQUFDQUFDcVNXMHIvY1NYekJIRW00UFcyYmQzalhBODE4MmZuYVFLClVIMUk4YVRhalp3M0VQNC9Ga0JQKzNJZU1RTk9qZHZzcTFoRWVlSjVNbWpYNVUyVFVKdVk3eXpnVkE5b0lNeVFQT1R0M0QKU2pJOGkwdHZENzZwVkJ4UlRYWVdDdm9YSWVMTWNSVzdab1R3OENwdGdrMkNIOWVOTEtUS3AxRnBVcXUzSHdJWi9DenlMdwpEczhaL3BXcC9hL0w0a0Z5ZTZpUmZvY1pNUVVZMFpWdWJTcloxenZsUGpkUlQvaXg0QmRFQ3YvRnNrRjcyekoyV0JGUjVDCnpndTQxTUFsZEpWYWh2T1JmczFHYVAwZlk2azc5K3VuRStPMERwOWludVdTb3luVzFjRmpBZmZ5MDlCY3N2NTNsK0krQlYKb1pYWnZoYzVuWHRFQW5DUlV0UDQ0SVlLaDdFamlKcFkraVVpYmpqa0Vsb3gyVGxnWUJScE13S1J1cS9GdzNsMnZCSzhmTw==
 EOF
