0fa5264e868a7f4ac89edb15f7a015c689b8f74f
Ric Featherstone <ric@control-plane.io>
2024-01-08 12:54:56 +0000

    style(terraform): terraform fmt

--- terraform/modules/ssh-config/ssh-config.tf
+++ terraform/modules/ssh-config/ssh-config.tf
@@ -1,16 +1,16 @@
 locals {
   ssh_config = templatefile("${path.module}/templates/ssh_config", {
-    bastion_ip = var.bastion_ip
-    instances = var.instances
-    ssh_user = var.ssh_user
-    ssh_force_tty = var.ssh_force_tty
+    bastion_ip        = var.bastion_ip
+    instances         = var.instances
+    ssh_user          = var.ssh_user
+    ssh_force_tty     = var.ssh_force_tty
     ssh_identity_file = var.ssh_identity_file
-    ssh_known_hosts = var.ssh_known_hosts_file
+    ssh_known_hosts   = var.ssh_known_hosts_file
   })
 }
 
 resource "local_file" "ssh_config" {
-  content = local.ssh_config
-  filename = format("%s/%s", var.ssh_config_dir, var.ssh_config_file)
+  content         = local.ssh_config
+  filename        = format("%s/%s", var.ssh_config_dir, var.ssh_config_file)
   file_permission = "0600"
 }
